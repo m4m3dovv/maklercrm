48dd0dea95bda50d4b7d9917187ad8bd436d6587
m4m3dovv <goldkassa@atomicmail.io>
2026-06-27 22:54:34 +0400

    Delete app directory

--- app/bot/states/customer_states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class AddCustomerStates(StatesGroup):
-    waiting_for_fullname = State()
-    waiting_for_phone = State()
-    waiting_for_budget = State()
-    waiting_for_district = State()
-    confirm = State()
--- app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.schemas.user import UserCreate, UserUpdate, UserResponse
-from app.schemas.property import PropertyCreate, PropertyUpdate, PropertyResponse
-from app.schemas.customer import CustomerCreate, CustomerUpdate, CustomerResponse
-
-__all__ = [
-    "UserCreate", "UserUpdate", "UserResponse",
-    "PropertyCreate", "PropertyUpdate", "PropertyResponse",
-    "CustomerCreate", "CustomerUpdate", "CustomerResponse"
-]
