fd742e0adf3d2a9c3f293eb7c2aa9358db4d9114
m4m3dovv <goldkassa@atomicmail.io>
2026-06-27 23:31:42 +0400

    Update property_states.py

--- app/bot/states/property_states.py
+++ app/bot/states/property_states.py
@@ -5,7 +5,11 @@ class AddPropertyStates(StatesGroup):
     waiting_for_district = State()
     waiting_for_address = State()
     waiting_for_room_count = State()
+    waiting_for_floor = State()          # Yeni
+    waiting_for_total_floors = State()   # Yeni
     waiting_for_area = State()
+    waiting_for_document_type = State()  # Yeni
+    waiting_for_owner_phone = State()    # Yeni
     waiting_for_price = State()
-    waiting_for_photo = State()
+    waiting_for_photo = State()          # Yeni (Şəkillər qəbul etmək üçün)
     confirm = State()
