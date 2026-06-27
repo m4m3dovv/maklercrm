b360eceec4b63ea491a89724856cfe24d332ab34
m4m3dovv <goldkassa@atomicmail.io>
2026-06-27 23:38:52 +0400

    Update property_states.py

--- app/bot/states/property_states.py
+++ app/bot/states/property_states.py
@@ -1,15 +1,17 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class AddPropertyStates(StatesGroup):
+    waiting_for_deal_type = State()      # Satılır yoxsa Kirayə
+    waiting_for_property_type = State()  # Bina, Həyət, Torpaq...
     waiting_for_title = State()
     waiting_for_district = State()
     waiting_for_address = State()
     waiting_for_room_count = State()
-    waiting_for_floor = State()          # Yeni
-    waiting_for_total_floors = State()   # Yeni
+    waiting_for_floor = State()
+    waiting_for_total_floors = State()
     waiting_for_area = State()
-    waiting_for_document_type = State()  # Yeni
-    waiting_for_owner_phone = State()    # Yeni
+    waiting_for_document_type = State()
+    waiting_for_owner_phone = State()
     waiting_for_price = State()
-    waiting_for_photo = State()          # Yeni (Şəkillər qəbul etmək üçün)
+    waiting_for_photos = State()         # ŞƏKİLLƏR YIĞIMI ÜÇÜN
     confirm = State()
